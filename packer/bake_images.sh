ded971414a1ef18370e55a24d6ca9f0cbfe69881
Victor Yang <yongweiy@google.com>
2018-09-16 12:04:17 -0700

    upate

--- packer/bake_images.sh
+++ packer/bake_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROJECT=$(gcloud config get-value project)
-GCP_ACCOUNT=/Users/yongweiy/.config/gcloud/pso-victory-cah-terraform.json
+GCP_ACCOUNT=$HOME/.config/gcloud/${PROJECT}-terraform.json
 packer build -only=googlecompute -var-file=variables.json -var gcp_project_id=$PROJECT -var gcp_account_file=$GCP_ACCOUNT elasticsearch6-node.packer.json
 packer build -only=googlecompute -var-file=variables.json -var gcp_project_id=$PROJECT -var gcp_account_file=$GCP_ACCOUNT  kibana6-node.packer.json
